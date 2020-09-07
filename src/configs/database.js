e30f692eb693896c74a5375093f47e35e3412490
AnirudhKumar10 <anirudh3104@gmail.com>
2020-09-07 23:10:40 +0530

    changs

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,5 +1,5 @@
 export default {
   remoteUrl:
-    "mongodb+srv://admin:admin123@cluster0-oalb1.mongodb.net/blogdb?retryWrites=true&w=majority",
+    "<REMOTE_URL>",
   localUrl: "mongodb://localhost/blogdb",
 };
